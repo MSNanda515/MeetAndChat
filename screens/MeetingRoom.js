95bf154ac620102c77fe9b6b4e9a89c51483b462
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-03-17 15:06:27 -0600

    Start with stack navigation

---
+++ screens/MeetingRoom.js
@@ -0,0 +1,19 @@
+import {View, StyleSheet, Text} from 'react-native';
+
+const MeetingRoom = () => {
+    return (
+        <View>
+            <Text>
+                Meeting Room
+            </Text>
+        </View>
+    )
+};
+
+export default MeetingRoom;
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
